74dff1f65d3d939499fb8d7c103fb934dbb34318
martinviellard <45688338+martinviellard@users.noreply.github.com>
2019-04-23 17:22:46 +0200

    Update Maincode.cpp

--- Maincode.cpp
+++ Maincode.cpp
@@ -7,6 +7,16 @@
 
 using namespace std;
 
+double negative_Value (double number) {
+   while (number < 0)
+	{
+		cout << "You seem to have entered a negative number" << endl;
+		cout << "Please enter the correct number.  " << endl;
+		cin >> number;
+	}
+    return number;
+}
+
 void fixedPrincipal (double loanPV, double principal, double rate, int period) {
     
     vector<vector<double> > table;
@@ -90,35 +100,24 @@ int main()
 	double periodicRate;
 	double principal, interestPayment, instalment;
 	double balance;
+    int optionLoan; 
+    int optionRate;
 
 
 	cout << "Welcome to Sorbank, a bank for and by Sorbonne students !" << endl;
 	cout << "This app is designed to generate with your help your loan payment schedule !" << endl;
 	cout << "Please enter the fixed rate as Annual Percentage Rate (APR) in %" << endl;
 	cin >> APR;
-	while (APR < 0)
-	{
-		cout << "You seem to have entered a negative rate, although this is possible in terms of real interest rates, Sorbank only proposes positive nominal rates." << endl;
-		cout << "Please enter the correct rate.  " << endl;
-		cin >> APR;
-	}
+	APR = negative_Value (APR);
 	APR = APR / 100;
 	cout << "Please enter the duration of the loan (in years) ";
 	cin >> loanDuration;
-	while (loanDuration < 0)
-	{
-		cout << "You seem to have entered a negative duration, please enter the correct number of years.";
-		cin >> loanDuration;
-	}
+	loanDuration = negative_Value(loanDuration);
 
 	cout << "Please enter the frequency of payment for the loan" << endl;
 	cout << "1 for annually, 2 for semi-annually, 4 for quarterly, and 12 for monthly  ";
 	cin >> frequency;
-	while (frequency != 1 && frequency != 2 && frequency != 4 && frequency != 12)
-	{
-		cout << "You have entered an incorrect frequency, please enter a correct one.  ";
-		cin >> frequency;
-	}
+	frequency = negative_Value(frequency); 
 	const int periodNb = frequency * loanDuration;
 	cout << "Okay ! Your loan will be rembursed over " << periodNb << " periods" << endl;
 
@@ -126,17 +125,31 @@ int main()
 
 	cout << "Enter the value of your loan (in USD) " << endl;
 	cin >> loanPV;
-	while (loanPV < 0)
-	{
-		cout << "You have entered a negative loan value, please enter a correct one.  " << endl;
-		cin >> loanPV;
-	}
+	loanPV = negative_Value(loanPV);
 
-	cout << "Right ! So you have borrowed " << loanPV << " $, which you will repay over " << periodNb << " periods." << endl;
-	principal = loanPV / periodNb;
+    cout << "Please select the type of loan you want to have" << endl;
+    cout << "For fixed principal type : 1" << endl;
+    cout << "For fixed instalments type : 2" << endl;
+	cin >> optionLoan;
 
-    fixedPrincipal(loanPV, principal, periodicRate, periodNb);
-    fixedInstalment(loanPV, periodicRate, periodNb);
+    if (optionLoan ==1 ){
+        cout << "You selected a fixed principal loan." << endl;
+        cout << "If you want fixed interest rate, type 1" << endl;
+        cout << "If you want floating rates, type 2" << endl;
+        cin >> optionRate;
+        
+        if (optionRate == 1) {
+            principal = loanPV / periodNb;
+            fixedPrincipal(loanPV, principal, periodicRate, periodNb);
+        }
+        if (optionRate==2) {
+
+        }
+        
+    }
+    if (optionLoan == 2) {
+        fixedInstalment(loanPV, periodicRate, periodNb);
+    }
 
 	return 0;
 }
